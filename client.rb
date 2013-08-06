95ce4605c08b4971c60a0ce2cd967e8d7646fd49
Johan Lindskogen <johan.lindskogen@gmail.com>
2013-08-06 18:50:31 +0200

    intelligent print of time

--- client.rb
+++ client.rb
@@ -2,7 +2,7 @@ require 'socket' # Sockets are in standard library
 require 'net/telnet'
 require 'json'
 
-hostname = '192.168.1.9'
+hostname = '192.168.1.10'
 port = 2000
 
 # "%X telnet 192.168.1.9 2000
@@ -12,7 +12,7 @@ server = TCPSocket.open hostname, port
 
 loop do
 	server_time = server.gets.chomp.to_i
-	puts Time.now.to_i - server_time
+	puts "#{server_time}: #{Time.now.to_i - server_time}"
 end
 puts server.gets.chomp
 
