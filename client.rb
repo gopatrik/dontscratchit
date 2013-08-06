8a5e93c047ba62f9519c336246fc59ba3172ea53
Patrik Gothe <patrik.got@gmail.com>
2013-08-06 15:48:42 +0200

    working with new system

--- client.rb
+++ client.rb
@@ -1,26 +1,20 @@
 require 'socket' # Sockets are in standard library
-# require 'json'
+require 'net/telnet'
+require 'json'
 
 hostname = '192.168.1.9'
 port = 2000
 
-def start_chat name, socket
-	puts "Connected!"
+# "%X telnet 192.168.1.9 2000
 
-	Thread.start(socket) do
-		loop { puts socket.gets.chomp } # Get data from server
-	end
-	
-	loop { socket.puts "#{name}: #{gets}" } # Send data to server
-end
+server = TCPSocket.open hostname, port
 
-def init
-	puts 'Connecting...'
-	socket = TCPSocket.open(hostname, port) # init socket
-	start_chat('Patrik', socket)
-end
+json_object = {
+	"name" => "Patrik",
+	"value" => "awesome"
+  }.to_json
 
-init
+server.puts json_object 
 
-socket.close # Close the socket when done
+puts server.gets.chomp
 
