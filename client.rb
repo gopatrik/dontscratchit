8250408e99e4e5bbcb917180873707361737d1a9
Patrik Gothe <patrik.got@gmail.com>
2013-08-06 22:59:29 +0200

    Classified client

--- client.rb
+++ client.rb
@@ -3,20 +3,24 @@ require 'net/telnet'
 require 'json'
 require_relative 'lib'
 
-hostname = '192.168.1.9'
-port = Util.port
-
-# "%X telnet 192.168.1.9 2000
-
-server = TCPSocket.open hostname, port
-
-array = Array.new
-
-loop do
-	server_time = server.gets.to_i
-	p server_time
-	array << Time.now.to_ms - server_time
-	puts "::#{(array.reduce(:+).to_f / array.size).to_i}"
+class Client
+	HOSTNAME = '192.168.1.9'
+	PORT = Util.port
+	def initialize
+		@server = TCPSocket.open HOSTNAME, PORT
+		@diff_list = Array.new
+		the_loop
+	end
+	
+	def the_loop
+		loop do
+			server_time = @server.gets.to_i
+			p server_time
+			@diff_list << Time.now.to_ms - server_time
+			puts "::#{@diff_list.average.to_i}"
+		end
+	end
+	
 end
-puts server.gets.chomp
 
+Client.new
