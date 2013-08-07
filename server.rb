7cafb7c04a9f67e4ffe67e9b6a2e1aeeb71e1e0c
Patrik Gothe <patrik.got@gmail.com>
2013-08-07 14:47:13 +0200

    Working server--somewhat

--- server.rb
+++ server.rb
@@ -40,6 +40,8 @@ class SimpleChatServer < EM::Connection
 
 	@@connected_clients = Hash.new
 	@started = false
+	@@time_started = nil
+	@@lap_time = 16 * (30/132)
 
 	# @@board = Board.new
 	
@@ -61,19 +63,28 @@ class SimpleChatServer < EM::Connection
 	def post_init
 		@@connected_clients[self] = {
 			:offset => 0,
-			:boards => [Board.new, Board.new]
+			:boards => [Board.new, Board.new],
+			:client_id => get_sockname.to_i.to_s
 		}
-		# 30 / 132 
+		# 30 / 132 = ett beat
+		# ett varv = 16 * (30/132) (sekunder)
 		self.send_line all_clients_data unless other_peers.empty?
 
 		self.the_loop unless @started
+		
 		puts "A client has connected..."
+		self.announce "CLIENTID:#{get_sockname}"
+		if @@connected_clients.size == 1
+			@@time_started = Time.now.to_ms
+			Thread.start { cycle }
+			
+		end
 	end
 
 	def all_clients_data
 		data = Array.new
 		other_peers.each do |scs, client|
-			index = { :client_id => client.object_id,
+			index = { :client_id => client[:client_id],
 								:boards => Array.new }
 			client[:boards].each do |board|
 				index[:boards] << board
@@ -88,23 +99,33 @@ class SimpleChatServer < EM::Connection
 		puts "A client has left..."
 	end
 
-	def next_beat_time
-		10 #current_beat
+	def next_lap
+		Time.now.to_ms - (@@time_started + @@lap_time * 1000)
 	end
 
 	def next_beat client
-		"DIFF:#{next_beat_time + @@connected_clients[client][:offset]}"
+		"DIFF:#{next_lap}" #+ @@connected_clients[client][:offset]}
+	end
+
+	def cycle
+		
+			loop do
+				puts "ASD = #{@@lap_time}"
+				puts "Hello: #{next_beat}"
+				sleep(@@lap_time)
+				@@time_started = Time.now.to_ms
+				self.announce "#{next_beat}", true
+			end
 	end
 
 	def receive_data(data)
-		if data.start_with? "TIME" then
+		if data.start_with? "TIME"
 			@@connected_clients[self][:offset] = Util.remove_prefix data
-			self.send_line "#{next_beat(self)}"
 		else
 			begin
 				data = JSON.parse(data.strip)
+				data[:client_id] = get_sockname.to_i.to_s
 				self.announce data.to_json
-
 				id = self.read_data data["diff"]
 
 				puts "#{data['name']} changed board #{id}!" unless data["name"].nil?
